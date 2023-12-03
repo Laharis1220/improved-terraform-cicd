876adcc13c4fdf0f6a1eca48693de6641f26fe1e
Laharis1220 <81207186+Laharis1220@users.noreply.github.com>
2023-12-03 17:54:01 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,10 @@
+/*terraform {
+      backend "s3" {
+      bucket         = "lahariimprovedterraformproject" 
+      key            = "lahari/terraform.tfstate"
+      region         = "us-east-1"
+      encrypt        = true
+      dynamodb_table = "terraform-lock"
+   }
+  }
+*/
